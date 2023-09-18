215019e8e8d2258f69d00942465a461477e560c8
Ashok Choudhary <ashoka8769@gmail.com>
2023-09-18 07:46:32 +0530

    update complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,131 @@
+import { useState } from "react";
+
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((i) => [...i, item]);
+  }
+
+  function handleDelete(id) {
+    setItems((items) => items.filter((i) => i.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) => items.map((item) => (item.id === id ? { ...item, packed: !item.packed } : item)));
+  }
+
+  function handleClearList() {
+    if (items.length === 0) return;
+    const confirmed = window.confirm("Are you sure you want to clear list?");
+    if (confirmed) setItems([]);
+  }
+
   return (
-    <div>
-      <h1>Hello React:)</h1>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        handleDelete={handleDelete}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1> 🌴 Far Away 🎒</h1>;
+}
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    onAddItems(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
+          <option value={i} key={i}>
+            {i}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..." value={description} onChange={(e) => setDescription(e.target.value)} />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items, handleDelete, onToggleItem, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed") sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item item={item} key={item.id} handleDelete={handleDelete} onToggleItem={onToggleItem} />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
+    </div>
+  );
+}
+
+function Item({ item, handleDelete, onToggleItem }) {
+  return (
+    <li>
+      <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => handleDelete(item.id)}>❌</button>
+    </li>
+  );
+}
+
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list </em>
+      </footer>
+    );
+  }
+  const numItem = items.length;
+  const packedItem = items.reduce((acc, item) => (item.packed ? ++acc : acc), 0);
+  const percentage = Math.round((packedItem / numItem) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈"
+          : ` 💼 You have ${numItem} items on your list, and you alredy packed ${packedItem} (${percentage}%)`}
+      </em>
+    </footer>
   );
 }
